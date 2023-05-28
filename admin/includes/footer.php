b98c8e5eb58ed748a45c45573aef78b30f721cf0
Pooja Wakchavare <134856684+Wakchavare@users.noreply.github.com>
2023-05-28 14:00:57 -0400

    Add files via upload

---
+++ admin/includes/footer.php
@@ -0,0 +1,7 @@
+<div>
+         
+            <div class="footer">
+                    <div class="clearfix"> </div>
+                        <p style="font-family: cursive;">Copyright (c) 2023 PoojaMuskanAsmitaPHP. All rights reserved.</p>
+            </div>
+        </div>
